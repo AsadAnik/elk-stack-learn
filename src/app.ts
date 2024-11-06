f44dea84d08dd0651581a55b7c3f43fbf2127c4c
asadaniktier5 <asad.hossain@tier5.in>
2024-11-06 19:35:32 +0600

    setup: setup for typescript and express backend app

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, Application } from "express";
 
-const app = express();
+const app: Application = express();
 
-app.get("/", function (_request: Request, response: Response) {
+app.get("/", function (_request: Request, response: Response): void {
     response.send("Hello all");
 });
 
-app.use('*', function (_request: Request, response: Response) {
+app.use('*', function (_request: Request, response: Response): void {
     response.status(404).send("Resource Not found!");
 });
 
-app.listen(3000, function () {
-    console.log("Server is running on port 3000");
+
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const HOST: string = process.env.HOST || "localhost";
+
+app.listen(PORT, HOST, function () {
+    console.log(`Server is running on ${HOST}:${PORT}`);
 });
